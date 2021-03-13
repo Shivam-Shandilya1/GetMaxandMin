5f98292171f0a6d2920b9a7d32056d1d3224a59c
Shivam Shandilya <f20202094@hyderabad.bits-pilani.ac.in>
2021-03-13 20:34:19 +0530

    Added c++file using single function for getting both min and max int.

---
+++ withsinglefunction.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void getMaxandMin(int number[],int sizes,int* minm,int* maxm)
+{
+
+     for(int i=0;i<sizes;i++)
+     {if(*maxm<number[i])
+     {
+         *maxm = number[i];
+     }
+     if(*minm>number[i])
+     {
+         *minm = number[i];
+     }
+     }
+}
+int main()
+{
+    int number[4];
+
+
+    for(int i=0;i<4;i++)
+    {
+        cout<<"Number : ";
+        cin>>number[i];
+    }
+
+    for(int i=0;i<4;i++)
+    {
+        cout<<number[i]<< "  ";
+    }
+    int maxm = number[0];
+    int minm = number[0];
+
+    getMaxandMin(number,4,&minm,&maxm);
+
+cout<<"Max number is:- "<<maxm <<endl;
+cout<<"Min number is:- "<<minm<<endl;
+    system("pause>0");
+}
